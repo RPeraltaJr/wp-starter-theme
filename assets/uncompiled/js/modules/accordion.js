bd6e89e752f29de1771508b86b6bf0f9a7d0dc2e
Rafael Peralta Jr <rperaltajr@gmail.com>
2020-05-13 12:47:51 -0400

    Create accordion.js

---
+++ assets/uncompiled/js/modules/accordion.js
@@ -0,0 +1,36 @@
+/*
+* --------------------------------------------------
+* Accordion
+*
+* Use [data-toggle="#element"] on a button
+* Use [data-target="#element"] on dropdown element
+* --------------------------------------------------
+*/
+
+// hide all data targets/dropdowns by default
+$('[data-target]').hide();
+
+// find target on click
+$('[data-toggle]').on('click keypress', function(){
+
+    // toggle class for clicked element
+    if( $(this).hasClass('data-toggle-active') ) {
+        $(this).removeClass('data-toggle-active');
+    } else {
+        $('[data-toggle]').removeClass('data-toggle-active');
+        $(this).addClass('data-toggle-active');
+    }
+
+    // get target element
+    let target = $(this).data('toggle');
+
+    // get target element's group if it exists
+    let group = $(`[data-target='${target}']`).data('group');
+
+    // hide all group items EXCEPT the current data target element
+    $('[data-group]').not($(`[data-target='${target}']`)).slideUp();
+
+    // toggle current data target element
+    $(`[data-target='${target}']`).slideToggle();
+    
+});
