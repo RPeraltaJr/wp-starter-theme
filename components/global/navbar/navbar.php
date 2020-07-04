2e8ded308fab5ed59c12e309c06a85f2c4a681a7
Rafael Peralta Jr <rperaltajr@gmail.com>
2020-07-03 22:01:59 -0400

    Update navbar.php

--- components/global/navbar/navbar.php
+++ components/global/navbar/navbar.php
@@ -1,3 +1,8 @@
-<nav class="navbar" aria-role="navigation" aria-label="site navigation">
-
-</nav>
+<section class="navbar" aria-role="navigation" aria-label="site navigation">
+  
+  <button id="navbarButton" class="hamburger hamburger--elastic" type="button" aria-label="Menu" aria-controls="navigation">
+      <span class="hamburger-box">
+          <span class="hamburger-inner"></span>
+      </span>
+  </button>
+</section>
