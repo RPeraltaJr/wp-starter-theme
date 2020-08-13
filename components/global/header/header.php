6a11b33e33da0f9b5763dc84a26ab7a765299eed
Rafael Peralta Jr <rperaltajr@gmail.com>
2020-08-13 13:13:46 -0400

    Update header.php

--- components/global/header/header.php
+++ components/global/header/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <?php if(strpos($_SERVER['HTTP_HOST'], 'productionsite.com') !== false): ?>
+    <?php if(strpos($_SERVER['HTTP_HOST'], 'productionsite.com') == false): ?>
       <meta name=’robots’ content=’noindex,follow’ />
     <?php endif; ?>
     <meta http-equiv="Cache-control" content="public">
