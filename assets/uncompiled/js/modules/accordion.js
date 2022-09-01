e8bda21b46be779c70e8729bddc28e7bea086062
Rafael Peralta Jr <rperaltajr@gmail.com>
2022-09-01 15:03:52 -0400

    Update accordion.js

--- assets/uncompiled/js/modules/accordion.js
+++ assets/uncompiled/js/modules/accordion.js
@@ -2,36 +2,68 @@
 * --------------------------------------------------
 * Accordion
 *
-* Use [data-toggle="#element"] on a button
-* Use [data-target="#element"] on dropdown element
-* Use [data-group="#group-name"] on element with [data-target] to assign a group name. Assigning group names will allow one accordion element to be open at a time.
+* Use [data-toggle="NAME"] or [data-show="NAME"] on a button
+* Use [data-target="NAME"] on dropdown element
+* Use [data-group="GROUP_NAME"] on element with [data-target] or [data-show] to assign a group name. Assigning group names will allow one accordion element to be open at a time.
+*
+* Add 'active' class to default dropdown element (element that has data-target)
 * --------------------------------------------------
 */
 
-// hide all data targets/dropdowns by default
-$('[data-target]').hide();
 
-// find target on click
+// * hide all data targets/dropdowns by default
+$('[data-target]:not(.active)').hide();
+
+// * find target on click
 $('[data-toggle]').on('click keypress', function(){
 
-    // toggle class for clicked element
-    if( $(this).hasClass('data-toggle-active') ) {
-        $(this).removeClass('data-toggle-active');
+    // * toggle class for clicked element
+    if( $(this).hasClass('active') ) {
+        $(this).removeClass('active');
     } else {
-        $('[data-toggle]').removeClass('data-toggle-active');
-        $(this).addClass('data-toggle-active');
+        $('[data-toggle]').removeClass('active');
+        $(this).addClass('active');
     }
 
-    // get target element
-    var target = $(this).data('toggle');
+    // * get target element
+    let target = $(this).data('toggle');
+    $('[data-target]').removeClass('active');
+    $('[data-target='+ target +']').addClass('active');
 
-    // get target element's group if it exists
-    var group = $('[data-target='+ target +']').data('group');
+    // * get target element's group if it exists
+    let group = $('[data-target='+ target +']').data('group');
 
-    // hide all group items EXCEPT the current data target element
-    $('[data-group]').not($('[data-target='+ target +']')).slideUp();
+    // * hide all group items EXCEPT the current data target element
+    $('[data-group]').not($('[data-target='+ target +']')).hide();
 
-    // toggle current data target element
-    $('[data-target='+ target +']').slideToggle();
+    // * toggle current data target element
+    $('[data-target='+ target +']').toggle();
+    
+});
+
+// * no toggle
+$('[data-show]').on('click keypress', function(){
+
+    // * toggle class for clicked element
+    if( $(this).hasClass('active') ) {
+        $(this).removeClass('active');
+    } else {
+        $('[data-show]').removeClass('active');
+        $(this).addClass('active');
+    }
+
+    // * get target element
+    let target = $(this).data('show');
+    $('[data-target]').removeClass('active');
+    $('[data-target='+ target +']').addClass('active');
+
+    // * get target element's group if it exists
+    let group = $('[data-target='+ target +']').data('group');
+
+    // * hide all group items EXCEPT the current data target element
+    $('[data-group]').not($('[data-target='+ target +']')).hide();
+
+    // * toggle current data target element
+    $('[data-target='+ target +']').show();
     
 });
