8b4a348f4aef42095914e5bacaf17ad06ad32036
Rafael Peralta Jr <rperaltajr@gmail.com>
2022-09-20 11:53:15 -0400

    Update header.php

--- components/global/header/header.php
+++ components/global/header/header.php
@@ -4,22 +4,10 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta http-equiv="Cache-control" content="public">
-    <meta name="description" content="<?php if(isset($page_setting->meta) && !empty($page_setting->meta->desc)): echo $page_setting->meta->desc; else: echo ""; endif; ?>">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
-    <meta name="theme-color" content="#FFFFFF"/>
-    
-    <meta property="og:type" content="website"/> 
-    <meta property="og:title" content="<?php if(isset($page_setting->meta) && !empty($page_setting->meta->title)): echo $page_setting->meta->title; else: echo ""; endif; ?>"/>
-    <meta property="og:url" content="<?php if($page_setting->name != 'index'): echo basename($_SERVER['REQUEST_URI']); endif; ?>" />
-    <meta property="og:image" content="<?php echo $img_path; ?>/og_img.jpg" /> 
-    <meta property="og:description" content="<?php if(isset($page_setting->meta) && !empty($page_setting->meta->desc)): echo $page_setting->meta->desc; else: echo ""; endif; ?>" />
-    <meta property="og:site_name" content="<?php echo get_bloginfo( 'name' ) . " - " ?? ''; ?><?php the_title(); ?>" />
 
     <title><?php echo get_bloginfo( 'name' ) . " - " ?? ''; ?><?php the_title(); ?></title>
-    <!-- <link rel="shortcut icon" href="<?php echo $img_path; ?>/favicon.ico" type="image/x-icon"> -->
+    <link rel="shortcut icon" href="<?php echo $img_path; ?>/favicon.png" type="image/png">
     
     <?php wp_head(); ?>
     
@@ -28,3 +16,5 @@
 
   </head>
   <body <?php body_class(); ?>>
+
+    <a href='#hero' class='sr-only'>Skip to main content</a>
